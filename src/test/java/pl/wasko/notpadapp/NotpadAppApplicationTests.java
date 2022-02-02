26c62b698906a69d7cead44bd3278a475a21151c
Michal <m.urbanek@wasko.pl>
2022-02-02 12:35:36 +0100

    Initial commit

---
+++ src/test/java/pl/wasko/notpadapp/NotpadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wasko.notpadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotpadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
